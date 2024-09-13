020527884d9218b5e18693bc6963c3fdcb8aee7c
bishwa <108havocx108@gmail.com>
2024-09-13 22:25:32 +0545

    js functionality added to redirect user to github on clicking github button

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('githubButton');
+
+        // Add an event listener to the button
+        button.addEventListener('click', function() {
+            // Redirect to GitHub
+            window.open('https://github.com/HavocXgrg', '_blank');
+        });
